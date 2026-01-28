315999bd00deb91cf1d46276bc7735a5ea204440
Takeuh Duplex <takeuhduplex2006@gmail.com>
2026-01-28 05:00:00 +0100

    Refactor uploadFile function and enhance image processing
    Refactor uploadFile function to improve readability and functionality. Added inpainting and OCR result handling.

--- app.py
+++ app.py
@@ -1,41 +1,48 @@
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import  Response
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-import easyocr
-import cv2
-import numpy as np
-import json
-from typing import Optional
+from fastapi.responses import FileResponse, StreamingResponse
+import cv2, zipfile, tempfile, json, numpy as np, easyocr, io
+from simple_lama_inpainting import SimpleLama
 
 app = FastAPI(debug=True)
+
 app.add_middleware(CORSMiddleware, 
-               allow_credentials=True, 
-               allow_headers=["*"], 
-               allow_methods=["*"],
-               allow_origins=["*"])
+                   allow_credentials=True, 
+                   allow_methods=["*"], 
+                   allow_origins=["*"],
+                   allow_headers=["*"],)
+@app.post("/")
+def hello():
+    return {
+        "message": "welcome to my API ",
+        "Content": "here you can do the inpainting of text with Lama and easyOCR",
+        "endpoints": ["/uploadfile"]
+    }
 
 @app.post("/uploadfile")
-async def uploadFile(workFile: UploadFile=File(...), 
-                    replaceName: Optional[str]=Form(None)):
+async def uploadFile(file : UploadFile=File(...)):
     
-    reader= easyocr.Reader(["fr", "en"])
+    # loading lama impainting
+    lama = SimpleLama()
     
-    if workFile.content_type.startswith("image/"):
+    # loading easyocr
+    reader = easyocr.Reader(["fr", "en"])
+    if file.content_type.startswith("image/"):
+        # create a buffer to read file
+        buffer = np.frombuffer(await file.read(), dtype=np.uint8)
         
-        #load image--------------------------------
-        image_bytes = await workFile.read()
-
-        # convert bytes to numpy array -----------------
-        np_array = np.frombuffer(image_bytes, np.uint8)
-
-        #decode image-----------------------------------
-        image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        #decode image
+        image = cv2.imdecode(buffer, cv2.IMREAD_COLOR)
+        image2 = image.copy()
         
-        #frame the text on the image------------------
-        result = reader.readtext(image)
+        # read text on the image
+        ocr_result = reader.readtext(image)
+        
+        #creating of mask
+        mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
+
         data = []
-        
-        for box, text, conf in result:
+        for box, text, conf in ocr_result:
             x_coords = [point[0] for point in box]
             y_coords = [point[1] for point in box]
 
@@ -43,56 +50,55 @@ async def uploadFile(workFile: UploadFile=File(...),
             y = int(min(y_coords))
             w = int(max(x_coords) - x)
             h = int(max(y_coords) - y)
-            
-            #Creating mask for remove a text to their position in the image 
-            mask = np.zeros(image.shape[:2], dtype=np.uint8)
-            
-            mask[y: y+h, x:x+w] = 255
-        
-            image = cv2.inpaint(image, mask, 3, cv2.INPAINT_TELEA)
 
-            #if you have set the replaceName in the form, then all the text in image will be replace by this text 
-            if replaceName:
-                image = cv2.putText(image, replaceName, (x, y), cv2.FONT_HERSHEY_SIMPLEX, (w/100), (0, 0, 0), 1)
+            mask = cv2.rectangle(mask, (x, y), (x+w, y+h), (255, 255, 255), -1)
             
-            else:
-                bg = cv2.imread("replace1.png")
-                bg_resize = cv2.resize(bg, (w, h))
-                image[y: y+h, x:x+w] = bg_resize
-            
-            #save text position of image 
+            # this is an OCR image result without inpainting
+            image2 = cv2.rectangle(image2, (x, y), (x+w, y+h), (255, 0, 0), 1)
+
             data.append({
                 "text": text,
-                "confidence" : float(conf),
+                "confidence": float(conf),
                 "x": x,
                 "y": y,
                 "w": w,
-                "h": h
+                "h": h,
             })
 
-        #encode image ------------------------------ 
-        _, encode_image = cv2.imencode(".png", image)
+        # inpainting
+        inpaint_result = lama(image, mask)
+
+        #encoding of data before send
+        inpaint_result = np.array(inpaint_result)
+
+        # image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
         
-        #send the image to frontend
-        return Response(content=encode_image.tobytes(), media_type="image/png", headers={
-            "image-data":  json.dumps(data),
-            "Access-Control-Expose-Headers": "image-data",
-        })
-    
-    else :
-        print("any image have been send please set an image before sent")
-        return {"message": "please send the image file "}
+        success1, inpaint_result_encode = cv2.imencode(".png", inpaint_result)
+        success2, ocr_result_encode = cv2.imencode(".png", image2)
 
+        if success1 and success2:
+            #zip files before send to client -----------------------
 
-@app.get("/")
-def home():
-    return {
-        "message" : "Welcome To My API OCR Make With Python",
-        "infos": {
-            "author": "Jovial Duplex",
-            "environement": "python3",
-            "Compatible-System": "All System are Compatible except the devices that don't have a browser",
-            "version": "1.0.0"
+            #creating of temporary zip file
+            # tempZip = tempfile.NamedTemporaryFile(delete=False, suffix=".zip")
+            zip_buffer = io.BytesIO()
+            with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zipf:
+                zipf.writestr("data.json", json.dumps(data))
+                zipf.writestr("ocr-result-image.png", ocr_result_encode.tobytes())
+                zipf.writestr("inpaint-result-image.png", inpaint_result_encode.tobytes())
+            
+            zip_buffer.seek(0)
+
+            return StreamingResponse(
+                zip_buffer,
+                media_type="application/zip",
+                headers={
+                    "Content-Disposition": "attachment; filename=data.zip"
+                }
+            )
+        # FileResponse(tempZip.name, filename="data.zip", media_type="application/zip")
+
+    else : 
+        return {
+            "message" : "please select right file before send"
         }
-    }
-
